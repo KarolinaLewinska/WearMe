a5a73011943b5b37dc9cc337ebebe073ee7d25d7
Karolina Lewińska <lewinskak95@gmail.com>
2022-05-02 15:02:33 +0200

    Add project files.

---
+++ WearMe/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using WearMe.ViewModels;
+using WearMe.Views;
+using Xamarin.Forms;
+
+namespace WearMe
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
